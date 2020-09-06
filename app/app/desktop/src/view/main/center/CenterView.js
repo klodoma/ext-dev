3581f586ac1390f7961239e32d5ae9c9afe18154
Andrei Bintintan <andy@interpid.eu>
2020-09-06 12:18:48 +0200

    Initial Commit

---
+++ app/app/desktop/src/view/main/center/CenterView.js
@@ -0,0 +1,6 @@
+Ext.define('App.view.main.center.CenterView', {
+	extend: 'Ext.Container',
+	xtype: 'centerview',
+	cls: 'centerview',
+	layout: 'card'
+});
---
+++ app/app/desktop/src/view/personnel/PersonnelViewModel.js
@@ -0,0 +1,7 @@
+Ext.define('App.view.personnel.PersonnelViewModel', {
+	extend: 'Ext.app.ViewModel',
+	alias: 'viewmodel.personnelviewmodel',
+	data: {
+		name: 'App'
+	}
+});
